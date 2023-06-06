9138a85883c515ad71d628e76f4cc4193ee3db84
Dion Dokter <diondokter@gmail.com>
2023-06-06 23:12:31 +0200

    Restrucure crate docs

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,5 +1,28 @@
-#![doc = include_str!("../../README.md")]
+//! See the [readme of the git repo](https://github.com/diondokter/traitify) for information about the crate
 
+/// ```rust
+/// use traitify::traitify;
+/// 
+/// struct Foo<BAR, const FIZZ: usize> {
+///     data: [BAR; FIZZ],
+/// };
+/// 
+/// #[traitify(FooDynFizz, dyn = [FIZZ])]
+/// #[traitify(FooDynBAR, dyn = [BAR])]
+/// #[traitify(FooDynAll, dyn = [FIZZ, BAR])]
+/// impl<BAR, const FIZZ: usize> Foo<BAR, FIZZ> {
+///     pub fn print_greeting(&self) {
+///         println!("Hallo vriendjes en vriendinnetjes!");
+///     }
+/// }
+/// 
+/// #[test]
+/// fn compiles() {
+///     let foo = Foo::<u8, 4> { data: [0; 4] };
+///     let dyn_foo: &dyn FooDynAll = &foo;
+///     dyn_foo.print_greeting();
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn traitify(
     args: proc_macro::TokenStream,
